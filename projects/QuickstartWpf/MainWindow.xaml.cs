7b2a90ac2b28417d303edc8b3d26e277cfb1fbfb
Scott W Harden <swharden@gmail.com>
2021-09-21 23:37:08 -0400

    projects: wpf quickstart

---
+++ projects/QuickstartWpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+namespace QuickstartWpf;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        MyGraphicsView.Drawable = new MyDrawable();
+        Loaded += (_, _) => MyGraphicsView.Invalidate();
+    }
+}
---
+++ projects/QuickstartWpf/MyDrawable.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.Maui.Graphics;
+using System;
+
+namespace QuickstartWpf;
+
+public class MyDrawable : IDrawable
+{
+    private readonly Random Rand = new();
+
+    public void Draw(ICanvas canvas, RectangleF dirtyRect)
+    {
+        canvas.FillColor = Color.FromArgb("#003366");
+        canvas.FillRectangle(dirtyRect);
+
+        for (int i = 0; i < 500; i++)
+        {
+            canvas.FillColor = Color.FromRgba(
+                r: Rand.NextDouble(),
+                g: Rand.NextDouble(),
+                b: Rand.NextDouble(),
+                a: .5);
+
+            canvas.FillCircle(
+                centerX: (float)Rand.NextDouble() * dirtyRect.Width,
+                centerY: (float)Rand.NextDouble() * dirtyRect.Height,
+                radius: (float)Rand.NextDouble() * 5 + 5);
+        }
+
+        canvas.FontSize = 36;
+        canvas.FontColor = Colors.White;
+        canvas.SetShadow(
+            offset: new SizeF(2, 2),
+            blur: 1,
+            color: Colors.Black);
+
+        canvas.DrawString(
+            value: "This is Maui.Graphics",
+            x: dirtyRect.Width / 2,
+            y: dirtyRect.Height / 2,
+            horizontalAlignment: HorizontalAlignment.Center);
+    }
+}
