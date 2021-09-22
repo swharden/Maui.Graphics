53d3929d7158d5de57dc28d3ed151e2991733bc0
Scott W Harden <swharden@gmail.com>
2021-09-22 00:30:32 -0400

    SiteBuilder: create project

---
+++ dev/SiteBuilder/FindFolder.cs
@@ -0,0 +1,35 @@
+using System.IO;
+
+public static class FindFolder
+{
+    public static DirectoryInfo Content() => FindAdjacentFolder("content");
+
+    public static DirectoryInfo Theme(string name)
+    {
+        string themes = FindAdjacentFolder("themes").FullName;
+        string theme = Path.Combine(themes, name);
+        if (!Directory.Exists(theme))
+            throw new ArgumentException($"theme not found: {theme}");
+        return new DirectoryInfo(theme);
+    }
+
+    private static DirectoryInfo FindAdjacentFolder(string targetFolderName, string startSearchAt = "./", int maxDepth = 20)
+    {
+        string currentPath = Path.GetFullPath(startSearchAt);
+        for (int i = 0; i < maxDepth; i++)
+        {
+            string possibleFolderPath = Path.Join(currentPath, targetFolderName);
+
+            if (Directory.Exists(possibleFolderPath))
+            {
+                return new DirectoryInfo(possibleFolderPath);
+            }
+
+            currentPath = Path.GetDirectoryName(currentPath)
+                ?? throw new InvalidOperationException($"Adjacent folder named '{targetFolderName}' not found at any level");
+        }
+
+        throw new InvalidOperationException($"Adjacent folder named '{targetFolderName}' not found in {maxDepth} levels");
+    }
+
+}
---
+++ dev/SiteBuilder/Program.cs
@@ -0,0 +1,5 @@
+﻿Statix.Generate.SingleArticlePages(
+    FindFolder.Content(),
+    FindFolder.Theme("primary"),
+    "https://github.com/swharden/Maui.Graphics/blob/main/projects/QuickstartWpf/QuickstartWpf.csproj",
+    "https://maui.graphics/");
