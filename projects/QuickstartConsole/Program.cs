3777b8b37faef0c53fcb364c6ac66b7fc1e6a793
Scott W Harden <swharden@gmail.com>
2021-10-27 20:04:22 -0400

    Projects: QuickstartConsole

---
+++ projects/QuickstartConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Graphics.Skia;
+
+BitmapExportContext bmp = SkiaGraphicsService.Instance.CreateBitmapExportContext(600, 400);
+SizeF bmpSize = new(bmp.Width, bmp.Height);
+ICanvas canvas = bmp.Canvas;
+
+canvas.FillColor = Colors.Navy;
+canvas.FillRectangle(0, 0, bmpSize.Width, bmpSize.Height);
+
+Random rand = new(0);
+for (int i = 0; i < 1000; i++)
+{
+    canvas.StrokeColor = Colors.White.WithAlpha(.5f);
+    canvas.DrawLine(
+        x1: (float)rand.NextDouble() * bmpSize.Width,
+        y1: (float)rand.NextDouble() * bmpSize.Height,
+        x2: (float)rand.NextDouble() * bmpSize.Width,
+        y2: (float)rand.NextDouble() * bmpSize.Height);
+}
+
+bmp.WriteToFile("console.png");
