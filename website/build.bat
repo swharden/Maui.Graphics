a2f564dafc2d3183081184429bdb1d2d518f6830
Scott W Harden <swharden@gmail.com>
2022-02-13 15:32:03 -0500

    dev: update hugo build scripts

---
+++ website/build.bat
@@ -0,0 +1,4 @@
+:: download Hugo and ensure it is in your path
+:: https://github.com/gohugoio/hugo/releases
+hugo --baseURL https://scottplot.net/ --cleanDestinationDir --enableGitInfo --minify --panicOnWarning --path-warnings --verbose --debug
+pause
